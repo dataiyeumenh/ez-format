ec7252c8e149fec263e8f1e928cce0f91f976806
anhlehoangg2003@gmail.com <anhlehoangg2003@gmail.com>
2026-03-28 11:53:26 +0700

    update backend

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 
-dotenv.config();
+require("dotenv").config();
 connectDB();
 
 const app = express();
